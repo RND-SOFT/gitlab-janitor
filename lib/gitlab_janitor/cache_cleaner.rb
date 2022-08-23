9f4db78dffe088fc71ee4207feb8d774a9469351
Samoilenko Yuri <kinnalru@gmail.com>
2022-08-23 10:27:05 +0300

    buildx prune

--- lib/gitlab_janitor/cache_cleaner.rb
+++ lib/gitlab_janitor/cache_cleaner.rb
@@ -10,13 +10,27 @@ module GitlabJanitor
     def do_clean(keep_size: '10G', remove: false)
       logger.info 'Removing cache...'
       if remove
-        out, _status = Open3.capture2e("docker builder prune --keep-storage #{keep_size} -f")
-        logger.info(out)
+        prune_builder
+        prune_buildx
       else
         logger.info 'Skip removal due to dry run'
       end
       out, _status = Open3.capture2e('docker system df')
       logger.info(out)
+    end
+    
+    def prune_builder
+      out, _status = Open3.capture2e("docker builder prune --keep-storage #{keep_size} -f")
+      logger.info(out)
+    rescue StandardError =>e
+      logger.warn("Unable to clean BUILDER: #{e.inspect}")
+    end
+    
+    def prune_buildx
+      out, _status = Open3.capture2e("docker buildx prune --keep-storage #{keep_size} -f")
+      logger.info(out)
+    rescue StandardError =>e
+      logger.warn("Unable to clean BUILDX: #{e.inspect}")
     end
 
   end
